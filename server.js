6f51ff48259cb75ee1e9f90cdd2fac6fd15a911e
iamrosan <roshanshrestha21.63@gmail.com>
2025-09-15 13:33:26 +0545

    chore: added server logs

--- server.js
+++ server.js
@@ -4,14 +4,16 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
+const webServer = process.env.APP_NAME;
 // Serve static files from the "public" folder
 app.use(express.static(path.join(__dirname, "public")));
 
 // Send index.html on root
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
+  console.log(`Request served by ${webServer}`);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`${webServer} running at http://localhost:${PORT}`);
 });
